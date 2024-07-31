8212325fca638f8288cc94eaaacf5ae54a77606e
londonrsteele <londonrsteele@gmail.com>
2024-07-30 19:58:30 -0500

    initial setup of python script file

---
+++ TennisAce/TennisAce.py
@@ -0,0 +1,11 @@
+# Filename : TennisAce.py
+# Author   : LR Steele
+# Info     : Coding project/objective for "Supervised Learning I: Regressors,
+#          : Classifiers, and Trees" section of Machine Learning Fundamentals
+#          : course in Machine Learning/AI Engineer Career Path on Codecademy.
+# Data     : see tennis_stats_README.md for more information
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
